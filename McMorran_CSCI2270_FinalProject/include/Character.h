bf5e6af6f55a2f9ddd6d691d259333a1bcd236c5
imcmorran <iamc5416@colorado.edu>
2016-04-20 22:03:46 -0600

    Updated Character library

--- McMorran_CSCI2270_FinalProject/include/Character.h
+++ McMorran_CSCI2270_FinalProject/include/Character.h
@@ -3,33 +3,43 @@
 
 #include <string>
 
-struct Item
-{
-    public:
-        std::string type;
-
-};
-
-struct Inventory
-{
-    public:
-        Item *items;
-        int size;
-        int stored;
-
-};
 
 class Character
 {
     public:
+        struct Item
+        {
+            std::string type;
+            int key;
+            int damage;
+        };
+
         Character();
         ~Character();
+        int moveUp();              // Will update the character's location
+        int moveDown();            // ''
+        int moveLeft();            // ''
+        int moveRight();           // ''
+        int addItem(Item);         // Adds an item to the inventory array
+        int removeItem(Item);      // Removes an item to the inventory array
+        void changeActiveLeft();    // Moves the pointer to the left
+        void changeActiveRight();   // Moves the pointer to the right
+        void updateHealth(int);     // updates health positively
+        void updateDamage(int);     // updates health negatively
+
     protected:
 
     private:
+        // Player Specs
         std::string _name;
         int _health;
-        Inventory _inventory;
+        int x;
+        int y;
+
+        // Inventory
+        Item * _inventory[10];
+        int _itemsStored;
+
 };
 
 #endif // CHARACTER_H
--- McMorran_CSCI2270_FinalProject/src/Character.cpp
+++ McMorran_CSCI2270_FinalProject/src/Character.cpp
@@ -1,11 +1,105 @@
 #include "Character.h"
+#include <string>
 
 Character::Character()
 {
     //ctor
+    x = -1;
+    y = -1;
+    _name = "Player";
+    _health = 100;
+    _itemsStored = 0;
+    for(int i=0; i<10; i++){
+        _inventory[i] = NULL;
+    }
 }
 
 Character::~Character()
 {
     //dtor
+    for(int i=0; i<10; i++){
+        if(_inventory[i]){
+            delete _inventory[i];
+        }
+    }
+}
+
+int Character::moveUp()              // Will update the character's location (Returns Y coordinate for vertical movement, X coordinate for horizontal movement, -1 for error)
+{
+    if(x<0 || y<0) return -1;
+
+    return --y;
+}
+
+int Character::moveDown()            // ''
+{
+    if(x<0 || y>0) return -1;
+
+    return ++y;
+}
+
+int Character::moveLeft()            // ''
+{
+    if(x<0 || y<0) return -1;
+
+    return --x;
+}
+
+int Character::moveRight()           // ''
+{
+    if(x<0 || y<0) return -1;
+
+    return ++x;
+}
+
+int Character::addItem(Item i)         // Adds an item to the inventory array
+{
+    Item * newItem = new Item;
+    newItem->damage = i.damage;
+    newItem->key    = i.key;
+    newItem->type   = i.type;
+
+    for(int i=0; i<10; i++){
+        if(!_inventory[i]){
+            _inventory[i] = newItem;
+            return 0;
+        }
+    }
+    return 101;                           // ERROR CODE 101: Inventory Full
+}
+
+int Character::removeItem(Item i)      // Removes an item to the inventory array
+{
+    for(int i=0; i<10; i++){
+        if(_inventory[i]){
+        if(_inventory[i]->type == i.type){
+            if(_inventory[i]->key == i.key){
+                delete _inventory[i];
+                _inventory[i] = NULL;
+                return 0;
+            }
+        }
+        }
+    }
+    return 102;                         // ERROR CODE 102: iTEM NOT FOUND
+}
+
+void Character::changeActiveLeft()    // Moves the pointer to the left
+{
+
+}
+
+void Character::changeActiveRight()   // Moves the pointer to the right
+{
+
+}
+
+void Character::updateHealth(int)     // updates health positively
+{
+
+}
+
+void Character::updateDamage(int)     // updates health negatively
+{
+
 }
